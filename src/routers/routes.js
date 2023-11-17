763ebce4bfef71f1301feff68a27b741c51f65f7
aniketSucomp <aniket.bobade@sucomptech.com>
2023-11-17 11:09:21 +0530

    update server message

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,18 +1,17 @@
-
 module.exports = (app) => {
-    app.use("/api", [
-     require("../validators-services-routes/user/routes"),
-     require("../validators-services-routes/field/routers"),
-     require("../validators-services-routes/subject/routes"),
-     require("../validators-services-routes/documents/routes"),
-    ]);
-    app.get("/", (req, res) => {
-      res.send("Hello from server");
+  app.use("/api", [
+    require("../validators-services-routes/user/routes"),
+    require("../validators-services-routes/field/routers"),
+    require("../validators-services-routes/subject/routes"),
+    require("../validators-services-routes/documents/routes"),
+  ]);
+  app.get("/", (req, res) => {
+    res.send("Server is working now");
+  });
+  app.all("*", (req, res) => {
+    return res.status(404).jsonp({
+      status: 404,
+      message: "route Not Found",
     });
-    app.all("*", (req, res) => {
-      return res.status(404).jsonp({
-        status: 404,
-        message: "route Not Found",
-      });
-    });
-  };
+  });
+};
